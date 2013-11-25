ae79afa8f59f319abff67910dfaa809374840842
Jeremy Francois <jeremy.francois@outlook.com>
2013-11-25 16:14:49 +0100

    Bug Fix and better matching method

--- src/main/java/fr/meijin/run4win/util/TournamentUtils.java
+++ src/main/java/fr/meijin/run4win/util/TournamentUtils.java
@@ -1,6 +1,7 @@
 package fr.meijin.run4win.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,59 +12,93 @@ import fr.meijin.run4win.model.Round;
 
 public class TournamentUtils {
 	
+	public static boolean matches (int roundNumber, List<Player> toMatch, List<Player> matchedPlayers,List<Round> lastRounds, List<Game> matched) {
+		
+		boolean foundMatch = false;
+		
+		while(toMatch.size()>0){
+			
+			foundMatch = false;
+			
+			for(int i = 0; i < toMatch.size(); i++){
+				for(int j = 0; j < toMatch.size(); j++ ){
+					if(i < toMatch.size() && j < toMatch.size()){
+						if(i != j && !havePlayedEachOther(lastRounds, toMatch.get(j), toMatch.get(i))){
+							Player p1 = toMatch.get(i);
+							Player p2 = toMatch.get(j);
+							Game g  = createMatchGame(roundNumber, lastRounds, p1, p2, matched);
+							matchedPlayers.add(p1);
+							matchedPlayers.add(p2);
+							
+							toMatch.remove(p1);
+							toMatch.remove(p2);
+							
+							matched.add(g);
+							foundMatch = true;
+							System.out.println("Matched : "+g.player1.nickname+" ["+g.player1.getPrestige()+"] vs "+g.player2.nickname+" ["+g.player2.getPrestige()+"] on "+g.tableNumber);
+							break;
+						}
+					}
+				}
+				if(foundMatch)
+					break;
+			}
+			
+			if(!foundMatch) return foundMatch;
+		}
+		
+		return foundMatch;
+	}
+	
 	public static Round doSingleMatch(int roundNumber, List<Round> lastRounds, List<Player> toMatch, List<Game> matched){
-		Round sr = new Round();
-		sr.roundNumber = roundNumber;
+		Round round = new Round();
+		round.roundNumber = roundNumber;
 		
 		List<Player> matchedPlayers = new ArrayList<Player>();
 		
-		while(toMatch.size()>0){
-			boolean foundMatch = false;
-			while (!foundMatch){
-				
-				for(int i = 0; i < toMatch.size(); i++){
-					for(int j = 0; j < toMatch.size(); j++ ){
-						if(i < toMatch.size() && j < toMatch.size()){
-							if(i != j && !havePlayedEachOther(lastRounds, toMatch.get(j), toMatch.get(i))){
-								Player p1 = toMatch.get(i);
-								Player p2 = toMatch.get(j);
-								Game g  = createMatchGame(sr.roundNumber, lastRounds, p1, p2, matched);
-								matchedPlayers.add(p1);
-								matchedPlayers.add(p2);
-								
-								toMatch.remove(p1);
-								toMatch.remove(p2);
-								
-								matched.add(g);
-								foundMatch = true;
-								System.out.println("Matched : "+g.player1.nickname+" ["+g.player1.getPrestige()+"] vs "+g.player2.nickname+" ["+g.player2.getPrestige()+"] on "+g.tableNumber);
-								break;
-							}
-						}
-					}
-					if(foundMatch)
-						break;
+		boolean foundMatch = false;
+		int pass = 0;
+		while (!foundMatch){
+			foundMatch = matches(roundNumber, toMatch, matchedPlayers, lastRounds, matched);
+			
+			if(pass < matched.size()) pass++;
+			
+			if(!foundMatch){
+				System.out.println("Error in matching, splitting and re-matching from sublist !");
+				int split = 0;
+				if(matched.size() % 2 == 0){
+					split = matched.size() / 2;
+				} else {
+					split = (matched.size() / 2)-pass;
 				}
 				
-				if(!foundMatch){
-					Player p1 = toMatch.get(0);
-					Player p2 = toMatch.get(1);
-					Game g  = createMatchGame(sr.roundNumber, lastRounds, p1, p2, matched);
-					matchedPlayers.add(p1);
-					matchedPlayers.add(p2);
-					
-					toMatch.remove(p1);
-					toMatch.remove(p2);
-					
-					matched.add(g);
-					foundMatch = true;
-					System.out.println("Matched : "+g.player1.nickname+" ["+g.player1.getPrestige()+"] vs "+g.player2.nickname+" ["+g.player2.getPrestige()+"] on "+g.tableNumber);
+				System.out.println("Splitting from "+split);
+				List<Game> matchedGames = matched.subList(0, split);
+				
+				
+				matchedPlayers.addAll(toMatch);
+				toMatch.clear();
+				
+				List<Player> sublistMatchedPlayers = new ArrayList<Player>();
+				for(Game g : matchedGames){
+					matchedPlayers.remove(g.player1);
+					matchedPlayers.remove(g.player2);
+					sublistMatchedPlayers.add(g.player1);
+					sublistMatchedPlayers.add(g.player2);
 				}
+				
+				matched = matchedGames;
+				toMatch = matchedPlayers;
+				matchedPlayers = sublistMatchedPlayers;
+				Collections.reverse(toMatch);
+				System.out.println("Size of matched game "+matched.size());
+				System.out.println("#players to match "+toMatch.size());
+				System.out.println("#matched players "+matchedPlayers.size());
 			}
 		}
 		
-		sr.games = matched;
-		return sr;
+		round.games = matched;
+		return round;
 	}
 	
 	private static Game createMatchGame(int roundNumber, List<Round> lastRounds, Player p1, Player p2, List<Game> matched) {
@@ -73,11 +108,13 @@ public class TournamentUtils {
 		sg.roundNumber = roundNumber;
 		
 		int tableNumber = 1;
-		for(Game g : matched)
+		for(Game g : matched){
+			System.out.println("Table Number = "+g.tableNumber);
 			if(g.tableNumber == tableNumber)
 				tableNumber++;
 			else
 				break;
+		}
 		
 		sg.tableNumber = tableNumber;
 		
@@ -114,7 +151,6 @@ public class TournamentUtils {
 					for(Game game : round.games){
 						if(player.id == game.player1.id || player.id == game.player2.id)
 							player.games.put(String.valueOf(game.roundNumber),game);
-						System.out.println("New record in player "+player.nickname+" with game on round "+game.roundNumber);
 					}
 				}
 			}
