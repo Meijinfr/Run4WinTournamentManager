b69c51c547f3a9f76fcaa8ae2613956b7dd75fe4
Jeremy Francois <jeremy.francois@outlook.com>
2014-09-05 10:58:48 +0200

    Version 2.3
    New IDs :
    - Chronos
    - Edward Kim
    - The Foundry
    - Gagarin
    - Masque
    - Shadow

--- src/main/java/fr/meijin/run4win/util/identity/CorporationIdentityEnum.java
+++ src/main/java/fr/meijin/run4win/util/identity/CorporationIdentityEnum.java
@@ -7,24 +7,29 @@ public enum CorporationIdentityEnum implements IdentityEnum {
 	
 	HB_ENGINEERING_THE_FUTURE				("engineering","Engineering the Future", "color : #ac69bb;","01054","Haas-Bioroid"),
 	HB_STRONGER_TOGETHER					("stronger","Stronger Together","color : #ac69bb;","02010","Haas-Bioroid"),
-	CUSTOM_BIOTICS							("custom","Custom Biotics","color : #ac69bb;","03002","Haas-Bioroid"),
 	CEREBRAL_IMAGING						("cerebral","Cerebral Imaging","color : #ac69bb;","03001","Haas-Bioroid"),
+	CUSTOM_BIOTICS							("custom","Custom Biotics","color : #ac69bb;","03002","Haas-Bioroid"),
 	NEXT_DESIGN								("next","NEXT Design","color : #ac69bb;","03003","Haas-Bioroid"),
+	THE_FOUNDRY								("foundry","The Foundry","color : #ac69bb;","06021","Haas-Bioroid"),
 	
 	JINTEKI_PERSONAL_EVOLUTION				("personal","Personal Evolution","color : #c53127;","01067","Jinteki"),
 	JINTEKI_REPLICATING_PERFECTION			("replicating","Replicating Perfection","color : #c53127;","02031","Jinteki"),
 	HARMONY_MEDTECH							("harmony","Harmony Medtech","color : #c53127;", "05001","Jinteki"),
 	TENNIN_INSTITUTE						("tennin","Tennin Institute","color : #c53127;", "05002","Jinteki"),
 	NISEI_DIVISION							("nisei","Nisei Division","color : #c53127;", "05003","Jinteki"),
+	CHRONOS_PROTOCOL						("chronos","Chronos Protocol","color : #c53127;", "00003","Jinteki"),
 	
 	NBN_MAKING_NEWS							("making","Making News","color : #ecaa2c;","01080","NBN"),
 	NBN_THE_WORLD_IS_YOURS					("world","The World is Yours","color : #ecaa2c;","02114","NBN"),
-	NEAR_EARTH								("nearearth","Near-Earth","color : #ecaa2c;","02114","NBN"),
+	NEAR_EARTH								("nearearth","Near-Earth","color : #ecaa2c;","06005","NBN"),
 	
 	WEYLAND_BUILDING_A_BETTER_WORLD			("building","Building a Better World","color : #4b796c;","01093","Weyland"),
 	WEYLAND_BECAUSE_WE_BUILT_IT				("because","Because We Built It","color : #4b796c;","02076","Weyland"),
-	BLUE_SUN								("blue","Blue Sun","color : #4b796c;","02076","Weyland"),
-	GRNDL									("grndl","GRNDL","color : #4b796c;","04097","Weyland");
+	GRNDL									("grndl","GRNDL","color : #4b796c;","04097","Weyland"),
+	BLUE_SUN								("blue","Blue Sun","color : #4b796c;","06068","Weyland"),
+	GAGARIN									("gagarin","Gagarin Deep Space","color : #4b796c;","07002","Weyland"),
+	
+	THE_SHADOW								("shadow","The Shadow","color : #ccc;","00005","Corporation");
 	
 	private String filename;
 	private String displayName;
--- src/main/java/fr/meijin/run4win/util/identity/RunnerIdentityEnum.java
+++ src/main/java/fr/meijin/run4win/util/identity/RunnerIdentityEnum.java
@@ -8,12 +8,13 @@ public enum RunnerIdentityEnum implements IdentityEnum {
 	NOISE									("noise","Noise","color : #e45f30;","01001","Anarch"),
 	WHIZZARD								("whizzard","Whizzard","color : #e45f30;","02001","Anarch"),
 	REINA_ROJA								("reina","Reina Roja","color : #e45f30;","04041","Anarch"),
+	EDWARD_KIM								("edward","Edward Kim","color : #e45f30;","07028","Anarch"),
 	
 	GABRIEL									("gabriel","Gabriel Santiago","color : #3e6ea9;","01017","Criminal"),
 	ANDROMEDA								("andromeda","Andromeda","color : #3e6ea9;","02083","Criminal"),
 	LARAMY_FISK								("laramy","Laramy Fisk","color : #3e6ea9;","00002","Criminal"),
 	SILHOUETTE								("silhouette","Silhouette","color : #3e6ea9;","05030","Criminal"),
-	KEN										("ken","Ken","color : #3e6ea9;","05029","Criminal"),
+	KEN										("ken","Ken 'Express' Tenma","color : #3e6ea9;","05029","Criminal"),
 	IAIN_STIRLING							("iain","Iain Stirling","color : #3e6ea9;","05028","Criminal"),
 	
 	KATE									("kate","Kate 'Mac' McCaffrey","color : #6b9141;","01033","Shaper"),
@@ -21,7 +22,9 @@ public enum RunnerIdentityEnum implements IdentityEnum {
 	RIELLE									("rielle","Rielle 'Kit' Peddler","color : #6b9141;","03028","Shaper"),
 	THE_PROFESSOR							("professor","The Professor","color : #6b9141;","03029","Shaper"),
 	EXILE									("exile","Exile","color : #6b9141;","03030","Shaper"),
-	NASIR_MEIDAN							("nasir","Nasir Meidan","color : #6b9141;","06017","Shaper");
+	NASIR_MEIDAN							("nasir","Nasir Meidan","color : #6b9141;","06017","Shaper"),
+	
+	THE_MASQUE								("masque","The Masque","color : #ccc;","00006","Runner");
 											
 	private String filename;
 	private String displayName;
